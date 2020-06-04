22ae321f1a0aa7c6c204895c07f664c8f2d569e4
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2020-06-04 09:47:50 -0300

    Update ExPy01.py

--- Exs Python/ExPy01.py
+++ Exs Python/ExPy01.py
@@ -1,9 +1,8 @@
-#Função print(): Serve para imprimir os argumentos passados a ela no terminal.
-
 var=str('Olá, mundo!')
 #Variável(var): Utilizada para armazenar valores em memória, elas nos permitem gravar e ler esses dados com facilidade a partir de um nome definido por nós.
 #Função str(): serve para produzir representações de valores que sejam legíveis para as pessoas.
 
 print('Olá, mundo!')
+#Função print(): Serve para imprimir os argumentos passados a ela no terminal.
 
 print(var)
