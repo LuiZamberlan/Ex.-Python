fac420060b8e642a43df5989364d6d822fb0f5f6
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2020-08-14 22:12:01 -0300

    Update ExPy016.py

--- Exs Python/ExPy016.py
+++ Exs Python/ExPy016.py
@@ -5,3 +5,7 @@ n = float(input('Type any value: '))
 
 print(f'O número {n} tem a parte inteira {trunc(n)}')
 #Função Trunc() coloca é uma forma de conseguir a parte inteira de um número, ele age como um ceil() para números negativos e como um floor() para números positivos.
+
+#outra forma: int()
+
+print(f'O número {n} tem a parte inteira {int(n)}')
