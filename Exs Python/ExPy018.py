3984fb5b7e92d5c4a6d5535bd49482dad154a503
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2020-08-15 20:41:44 -0300

    Create ExPy018.py

---
+++ Exs Python/ExPy018.py
@@ -0,0 +1,14 @@
+from math import cos, tan, sin, radians
+
+ang = float(input('Digite o valor do ângulo: '))
+
+#Função sin() calcula o seno com o valor do ângulo recebido
+print(f'O seno do ângulo de {ang} é {sin(radians(ang)):.2f}')
+
+#Função cos() calcula o cosseno com o valor do ângulo recebido
+print(f'O seno do ângulo de {ang} é {cos(radians(ang)):.2f}')
+
+#Função tan() calcula a tangente com o valor do ângulo recebido
+print(f'O seno do ângulo de {ang} é {tan(radians(ang)):.2f}')
+
+#Função radians() transforma o valor do ângulo em valor radiano
