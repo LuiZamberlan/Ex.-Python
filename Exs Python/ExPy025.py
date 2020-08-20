2274a8612632cd9e9568e1112d96e5a3c3f03f76
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2020-08-20 20:17:53 -0300

    Update ExPy025.py

--- Exs Python/ExPy025.py
+++ Exs Python/ExPy025.py
@@ -1,4 +1,4 @@
-n = str(input('Qual seu nome completo? '))
+n = str(input('Qual seu nome completo? ')).strip().lower()
 
 print(f'Seu nome tem "Dilma"? {"Silva" in n}')
 #O operador 'in' verifica se tem "Silva" na variável 'n'
