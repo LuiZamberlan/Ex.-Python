f9dc461a01f2af47c370d8404ec972b5778160dc
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2020-08-20 21:15:58 -0300

    Create ExPy026.py

---
+++ Exs Python/ExPy026.py
@@ -0,0 +1,10 @@
+f = str(input('Digite uma frase: ')).strip().lower()
+
+print(f'Quantas vezes aparece a letra "a"? {f.count("a")}')
+#O método count() contará quantos 'a's tem na string
+
+print(f'Qual foi a 1° vez que apareceu? Na {f.find("a") + 1}° Posição')
+#O método find() indicará qual o index do 1° 'a' na string
+
+print(f'Qual foi a ultima vez que apareceu? Na {f.rfind("a")+1}° Posição')
+#O método rfind() executa o mesmo comando do find(), mas começando pela direita
