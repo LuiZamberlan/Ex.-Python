673da768ed058da7a635adcbd1107de0ee4612ab
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2020-08-23 07:24:02 -0300

    Create ExPy031.py

---
+++ Exs Python/ExPy031.py
@@ -0,0 +1,9 @@
+k = float(input('Qual a distância da viagem? '))
+
+if k <= 200:
+    print(f'O preço da passagem é de R${k * 0.50:.2f}')
+    #O preço para viagens curtas o custo é de R$0,50 por km.
+
+else:
+    print(f'O preço da passagem é de R${k * 0.45:.2f}')
+    #Para viagens mais longas o custo é de R$0,45 por km.
