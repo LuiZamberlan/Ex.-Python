def616fd34c3e05c925edf05d88aad7e977da22e
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2020-08-23 07:23:57 -0300

    Update ExPy026.py

--- Exs Python/ExPy026.py
+++ Exs Python/ExPy026.py
@@ -6,5 +6,5 @@ print(f'Quantas vezes aparece a letra "a"? {f.count("a")}')
 print(f'Qual foi a 1° vez que apareceu? Na {f.find("a") + 1}° Posição')
 #O método find() indicará qual o index do 1° 'a' na string
 
-print(f'Qual foi a ultima vez que apareceu? Na {f.rfind("a")+1}° Posição')
+print(f'Qual foi a ultima vez que apareceu? Na {f.rfind("a") + 1}° Posição')
 #O método rfind() executa o mesmo comando do find(), mas começando pela direita
